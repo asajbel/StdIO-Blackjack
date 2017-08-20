8a8e998289ca5a188b66c05707b3dd005024f499
asajbel <serdan66@gmail.com>
2017-08-19 21:42:55 -0700

    Deck builds

--- Blackjack.js
+++ Blackjack.js
@@ -5,7 +5,55 @@ function print (text, where = "output") {
 function read(e, value, id) {
   if (e.keyCode === 13) {
     e.preventDefault();
-    print(value);
+    playerAction(value);
     document.getElementById(id).value = '';
+  }
+}
+
+function newDeck() {
+  var cards = [];
+  var suits = ['hearts','spades','diamonds','clubs'];
+  for (var i = 0; i < 4; i++) {
+    for (var j = 1; j <= 13; j++){
+      switch (j){
+        case 1:
+          cards.push([j,'Ace',suits[i]]);
+          break;
+        default:
+          cards.push([j,j,suits[i]]);
+          break;
+        case 11:
+          cards.push([j,'Jack',suits[i]]);
+          break;
+        case 12:
+          cards.push([j,'Queen',suits[i]]);
+          break;
+        case 13:
+          cards.push([j,'King',suits[i]]);
+          break;
+        }
+    }
+  }
+  return cards;
+}
+
+var deck = newDeck();
+
+function shuffle() {
+
+}
+
+function playerAction(bet) {
+  var b = bet[0];
+  print(b);
+  switch (b){
+    case "d":
+    break;
+    case "h":
+    break;
+    case "s":
+    break;
+    case '/':
+    break;
   }
 }
